f1f7f878a34f46d8c577210a823759051be60d83
umrazhussain <47004577+umrazhussain@users.noreply.github.com>
2020-04-01 19:38:13 +0530

    ML model to predict house pricing
    ML model to predict house pricing with dataSet

---
+++ housePrice.py
@@ -0,0 +1,58 @@
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load in 
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+
+from subprocess import check_output
+print(check_output(["ls", "../input"]).decode("utf8"))
+
+# Any results you write to the current directory are saved as output.
+
+#Import Libraries
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+np.set_printoptions(threshold=np.nan)
+
+
+#Importing DataSet 
+dataset = pd.read_csv("../input/kc_house_data.csv")
+space=dataset['sqft_living']
+price=dataset['price']
+
+x = np.array(space).reshape(-1, 1)
+y = np.array(price)
+
+#Splitting the data into Train and Test
+from sklearn.cross_validation import train_test_split 
+xtrain, xtest, ytrain, ytest = train_test_split(x,y,test_size=1/3, random_state=0)
+
+
+#Fitting simple linear regression to the Training Set
+from sklearn.linear_model import LinearRegression 
+regressor = LinearRegression()
+regressor.fit(xtrain, ytrain)
+
+#Predicting the prices
+pred = regressor.predict(xtest)
+
+#Visualizing the training Test Results 
+plt.scatter(xtrain, ytrain, color= 'red')
+plt.plot(xtrain, regressor.predict(xtrain), color = 'blue')
+plt.title ("Visuals for Training Dataset")
+plt.xlabel("Space")
+plt.ylabel("Price")
+plt.show()
+
+#Visualizing the Test Results 
+plt.scatter(xtest, ytest, color= 'red')
+plt.plot(xtrain, regressor.predict(xtrain), color = 'blue')
+plt.title("Visuals for Test DataSet")
+plt.xlabel("Space")
+plt.ylabel("Price")
+plt.show()
